66e48660beb6f0409f856ae16b55e549679c6ec9
boboyevnurbek <boboyevnurbek95@gmail.com>
2022-10-05 17:18:36 +0500

    5-commit in lesson18 annotation task solution

--- Lesson18_Annotations_and_ThreadPool/src/main/java/com/company/annotations/entity/Student.java
+++ Lesson18_Annotations_and_ThreadPool/src/main/java/com/company/annotations/entity/Student.java
@@ -2,17 +2,30 @@ package com.company.annotations.entity;
 
 import lombok.*;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
+@ClassAnnotation
 public class Student {
+
     private String fullName;
     private String university;
     private int course;
 
+    @InfoAnnotation(number = -3)
     public void info(){
         System.out.println(this);
     }
+}
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@interface ClassAnnotation{
 }
--- Lesson18_Annotations_and_ThreadPool/src/main/java/com/company/annotations/examples/App4.java
+++ Lesson18_Annotations_and_ThreadPool/src/main/java/com/company/annotations/examples/App4.java
@@ -1,9 +1,43 @@
 package com.company.annotations.examples;
 
+import com.company.annotations.entity.InfoAnnotation;
+import com.company.annotations.entity.Student;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class App4 {
     public static void main(String[] args) {
         // 1. create annotation for class with named 'ClassAnnotation'
         // 2. create annotation for info() method with named 'InfoAnnotation(number=3)'
         // 3. invoke info() method by number InfoAnnotation
+
+        // 12 minutes
+
+        Student student = new Student("Otabek", "IT PARK UNIVERSITY", 1);
+
+        Annotation[] annotations = Student.class.getAnnotations();
+        for (Annotation annotation : annotations) {
+            System.out.println(annotation);
+        }
+
+        for (Method method : Student.class.getDeclaredMethods()) {
+            InfoAnnotation[] infoAnnotations = method.getAnnotationsByType(InfoAnnotation.class);
+            for (InfoAnnotation annotation : infoAnnotations) {
+                System.out.println(annotation);
+                for (int i = 0; i < annotation.number(); i++) {
+                    try {
+                        method.invoke(student);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
+
     }
 }
