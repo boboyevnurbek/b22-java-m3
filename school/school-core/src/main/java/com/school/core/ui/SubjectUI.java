0f2ebf3076f857e0eb5ec80d288cfd02b35b5fa3
boboyevnurbek <boboyevnurbek95@gmail.com>
2022-10-07 18:47:43 +0500

    5-commit in lesson20 app school

--- school/school-core/src/main/java/com/school/core/ui/SubjectUI.java
+++ school/school-core/src/main/java/com/school/core/ui/SubjectUI.java
@@ -1,11 +1,14 @@
 package com.school.core.ui;
 
 import com.school.core.Application;
+import com.school.core.io.InOut;
 import com.school.core.util.ScannerUtil;
+import com.school.domain.db.Database;
 import com.school.domain.entity.Subject;
 import com.school.domain.payload.Result;
 import com.school.domain.service.SubjectService;
 
+import java.util.InputMismatchException;
 import java.util.List;
 
 
@@ -15,70 +18,88 @@ public class SubjectUI {
 
         while (true) {
 
-            System.out.println();
-            String choice = getSubjectMenu();
+            InOut.printText("", InOut.ANSI_RESET);
+            try {
+                String choice = getSubjectMenu();
 
-            if (choice.equals("0")) break;
+                if (choice.equals("0")) break;
 
-            switch (choice) {
-                case "1" -> createSubject();
-                case "2" -> readSubjects();
-                case "3" -> editSubject();
-                case "4" -> deleteSubject();
+                switch (choice) {
+                    case "1" -> createSubject();
+                    case "2" -> readSubjects();
+                    case "3" -> editSubject();
+                    case "4" -> deleteSubject();
+                }
+            }catch (InputMismatchException e){
+                ScannerUtil.SCANNER_NUM.nextLine();
             }
         }
 
     }
 
     private static void deleteSubject() {
+        boolean exist = readSubjects();
+        if(!exist) return;
 
+        InOut.printText("Enter subject id: ", InOut.ANSI_BLUE);
+        Integer subjectId = ScannerUtil.SCANNER_NUM.nextInt();
+
+        Result result = Application.subjectService.deleteSubject(subjectId);
+
+        InOut.printText(result.getMessage(), result.isSuccess() ? InOut.ANSI_GREEN : InOut.ANSI_RED);
     }
 
     private static void editSubject() {
 
+        boolean exist = readSubjects();
+        if(!exist) return;
+
+        InOut.printText("Enter subject id: ", InOut.ANSI_BLUE);
+        Integer subjectId = ScannerUtil.SCANNER_NUM.nextInt();
+        InOut.printText("New name: ", InOut.ANSI_BLUE);
+        String newName = ScannerUtil.SCANNER_STR.nextLine();
+        Result result = Application.subjectService.editSubject(subjectId, newName);
+
+        InOut.printText(result.getMessage(), result.isSuccess() ? InOut.ANSI_GREEN : InOut.ANSI_RED);
     }
 
-    private static void readSubjects() {
+    private static boolean readSubjects() {
 
         List<Subject> subjectList = Application.subjectService.getSubjectList();
 
         if (subjectList.isEmpty()) {
 
-            System.out.println("No subjects added yet.");
-
+            InOut.printText("No subjects added yet.", InOut.ANSI_RED);
+            return false;
         } else {
 
             for (Subject subject : subjectList) {
-                System.out.println("Subject name: " + subject.getName());
+                InOut.printText("Id: " + subject.getId() + ", Subject name: " + subject.getName(), InOut.ANSI_YELLOW);
             }
 
         }
+        return true;
     }
 
     private static void createSubject() {
 
-        System.out.print("Enter name for a subject: ");
+        InOut.printText("Enter name for a subject: ", InOut.ANSI_BLUE);
         String name = ScannerUtil.SCANNER_STR.nextLine();
 
 
         Result result = Application.subjectService.addSubject(name);
 
-        if (result.isSuccess()) {
-            System.out.println(result.getMessage());
-        } else {
-            System.err.println(result.getMessage());
-        }
+        InOut.printText(result.getMessage(), result.isSuccess() ? InOut.ANSI_GREEN : InOut.ANSI_RED);
 
     }
 
-
     private static String getSubjectMenu() {
-        System.out.println("1. Create new subject");
-        System.out.println("2. Read subject list");
-        System.out.println("3. Edit subject");
-        System.out.println("4. Delete subject ");
-        System.out.println("0. Back");
-        System.out.print("Enter operation number: ");
+        InOut.printText("1. Create new subject", InOut.ANSI_BLUE);
+        InOut.printText("2. Read subject list", InOut.ANSI_BLUE);
+        InOut.printText("3. Edit subject", InOut.ANSI_BLUE);
+        InOut.printText("4. Delete subject ", InOut.ANSI_BLUE);
+        InOut.printText("0. Back", InOut.ANSI_BLUE);
+        InOut.printText("Enter operation number: ", InOut.ANSI_BLUE);
         return ScannerUtil.SCANNER_STR.nextLine();
     }
 }
--- school/school-domain/src/main/java/com/school/domain/service/SubjectService.java
+++ school/school-domain/src/main/java/com/school/domain/service/SubjectService.java
@@ -9,20 +9,44 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class SubjectService {
+    public Result editSubject(int subjectId, String newName) {
+
+        Subject subject = getSubjectById(subjectId);
+        if (subject == null || newName.isBlank()) {
+            return new Result("A subject with this id doesn't exist or name is empty", false);
+        }
+
+        if (subject.getName().equalsIgnoreCase(newName)) {
+            subject.setName(newName);
+            return new Result("A subject has been successfully edited", true);
+        }
+
+        Subject subject2 = Database.SUBJECTS.stream()
+                .filter(subject1 -> subject1.getName().equalsIgnoreCase(newName))
+                .findFirst().orElse(null);
+
+        if (subject2 != null)
+            return new Result("Subject with this name already exists", false);
+
+        subject.setName(newName);
+        return new Result("A subject has been successfully edited", true);
+    }
+
     public Result addSubject(String name) {
 
         if (name == null || name.isBlank())
             return new Result("name is required", false);
 
+        String str = name.trim();
         Subject subject = Database.SUBJECTS.stream()
-                .filter(sbj -> sbj.getName().equalsIgnoreCase(name))
+                .filter(sbj -> sbj.getName().equalsIgnoreCase(str))
                 .findFirst()
                 .orElse(null);
 
         if (Objects.nonNull(subject))
             return new Result("Subject exist with this name", false);
 
-        subject = new Subject(Database.SUBJECTS.size() + 1, name);
+        subject = new Subject(Database.SUBJECTS.size() + 1, str);
         Database.SUBJECTS.add(subject);
 
         return new Result("Successfully created", true);
@@ -32,5 +56,25 @@ public class SubjectService {
         return Database.SUBJECTS.stream()
                 .filter(subject -> !subject.isDeleted())
                 .collect(Collectors.toList());
+    }
+
+    public Subject getSubjectById(Integer subjectId) {
+        return Database.SUBJECTS.stream()
+                .filter(subject -> subject.getId().equals(subjectId) && !subject.isDeleted())
+                .findFirst().orElse(null);
+    }
+
+    public Result deleteSubject(Integer subjectId) {
+        Subject subject = getSubjectById(subjectId);
+
+        if (subject == null) {
+            return new Result("Subject not found", false);
+        }
+
+        if (subject.isDeleted())
+            return new Result("Subject already deleted", false);
+
+        subject.setDeleted(true);
+        return new Result("Subject has been successfully deleted", true);
     }
 }
